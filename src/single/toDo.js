91c814d97b69e328303593467e3d45cde542217b
WeLa <laurin.weber@students.hevs.ch>
2020-04-11 16:06:37 +0200

    ToDos

---
+++ src/single/toDo.js
@@ -0,0 +1,7 @@
+/*
+-> inputHandler AI -> abwechlungsweise nach oben unten, Random zahl einbauen.. sonst zu einfach
+-> wall -> ACHTUNG, ball kann gefangen werden und herum flippern
+-> ball -> maximale geschwindigkeit definieren, ansonsten wenn zuschnell
+werden die paddles übersprungen und es gibt einen Punkt.. bug
+
+ */
